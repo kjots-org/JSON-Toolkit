e81241b8d279b45da608770cc239d93f698618e3
kjots@kjots.org <kjots@kjots.org>
2010-06-29 00:14:52 +1000

    Updated PartialJsonContentHandler to throw UnsupportedOperationException instead of asserting false.

--- src/main/java/org/kjots/json/content/shared/PartialJsonContentHandler.java
+++ src/main/java/org/kjots/json/content/shared/PartialJsonContentHandler.java
@@ -18,8 +18,8 @@ package org.kjots.json.content.shared;
 /**
  * Partial JSON Content Handler.
  * <p>
- * This class implements {@link JsonContentHandler} and uses assertions to
- * cause every method to fail (if assertions are enabled).
+ * This class implements {@link JsonContentHandler} and uses {@link UnsupportedOperationException}s
+ * to cause every method to fail.
  * <p>
  * It is intended that this class should be used as the base for classes that
  * only concern themselves with a part of the {@link JsonContentHandler}
@@ -35,11 +35,6 @@ package org.kjots.json.content.shared;
  * method will never be invoked - thus such a class need not provide an
  * implementation of that method.
  * <p>
- * If an unimplemented method is invoked (assuming that the implementing class
- * is correctly overriding every method it is supposed to), that indicates an
- * error in the generator of JSON content events, and the assertions feature of
- * the Java language may be used to trap such behaviour.
- * <p>
  * Created: 15th February 2010.
  *
  * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
@@ -50,7 +45,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void startJson() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -58,7 +53,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void endJson() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -66,7 +61,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void startObject() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -74,7 +69,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void endObject() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -82,7 +77,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void startArray() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -90,7 +85,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void endArray() {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -100,7 +95,7 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void memberName(String name) {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
   
   /**
@@ -110,6 +105,6 @@ public abstract class PartialJsonContentHandler implements JsonContentHandler {
    */
   @Override
   public void primitive(Object value) {
-    assert false : "Method not implemented";
+    throw new UnsupportedOperationException();
   }
 }
