9aef05805d7d6cbb2d97dc606e2591a0725a6f33
kjots@kjots.org <kjots@kjots.org>
2011-05-29 00:57:08 +1000

    Added JSON Object Scala module.

---
+++ json-object/scala/src/test/scala/org/kjots/json/object/scala/ScalaJsonObjectTestSuite.scala
@@ -0,0 +1,37 @@
+/* 
+ * Copyright © 2011 Karl J. Ots <kjots@kjots.org>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kjots.json.`object`.scala
+
+import com.google.inject.Guice
+
+import org.junit.Before
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+/**
+ * Scala JSON Object Test Suite.
+ * <p>
+ * Created: 28th May 2011.
+ *
+ * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
+ * @since 1.2
+ */
+@RunWith(classOf[Suite])
+@SuiteClasses(Array(
+))
+class ScalaJsonObjectTestSuite {}
