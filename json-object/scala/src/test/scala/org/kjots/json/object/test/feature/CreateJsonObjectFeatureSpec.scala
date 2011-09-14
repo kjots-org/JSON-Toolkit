0423dd05830166a14e0758855e5bcf3593f57f90
Karl J. Ots <kjots@kjots.org>
2011-09-14 13:39:33 +1000

    Updated scala module to use scalatest and added Create JSON Object Feature Specification.

---
+++ json-object/scala/src/test/scala/org/kjots/json/object/test/feature/CreateJsonObjectFeatureSpec.scala
@@ -0,0 +1,22 @@
+package org.kjots.json.`object`.test.feature
+
+import org.junit.runner.RunWith
+
+import org.scalatest.FeatureSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * Create JSON Object Specification.
+ * <p>
+ * Created: 14th September 2011.
+ * 
+ * @author <a href="mailto:kjots@kjots.org">Karl J. Ots &lt;kjots@kjots.org&gt;</a>
+ * @since 1.2
+ */
+@RunWith(classOf[JUnitRunner])
+class CreateJsonObjectFeatureSpec extends FeatureSpec {
+  feature("The user can create a new JSON Object") {
+    scenario("A new JSON Object is created") (pending)
+    scenario("A new JSON Object has no properties") (pending)
+  }
+}
