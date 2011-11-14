5d4ce65677e225d3599dbffb966a847a62cec823
Karl J. Ots <kjots@kjots.org>
2011-11-14 20:36:54 +1100

    Updated org.kjots.json-toolkit.json-object.scala module to use failsafe plugin to run feature specifications.

---
+++ org.kjots.json-toolkit.json-object.scala/src/test/scala/org/kjots/json/object/test/feature/ScalaJsonObjectFeatureSuite.scala
@@ -0,0 +1,11 @@
+package org.kjots.json.`object`.test.feature
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+@RunWith(classOf[Suite])
+@SuiteClasses(Array(
+  classOf[CreateJsonObjectFeatureSpec]
+))
+class ScalaJsonObjectFeatureSuite {}
